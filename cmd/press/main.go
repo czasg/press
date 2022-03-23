c4db7f78ea007d990627e45e33a6c26372321d9d
陈子昂 <chenziang@antiy.cn>
2022-03-23 18:19:06 +0800

    feat: init

--- cmd/press/main.go
+++ cmd/press/main.go
@@ -10,6 +10,45 @@ import (
     "syscall"
 )
 
+func CreateYaml() {
+    f, err := os.Create("press.yaml")
+    if err != nil {
+        return
+    }
+    defer f.Close()
+    f.WriteString(`---
+version: "1"
+metadata:
+  name: "press"
+steps:
+  - name: "压力测试"
+    logInterval: 1
+    threadGroup:
+      thread: 2
+      threadRampUp: 1
+      duration: 10
+    http:
+      url: "http://www.baidu.com"
+      method: "POST"
+      headers:
+        content-type: "application/json"
+      body: |
+        {
+          "hello":"press"
+        }
+    assert:
+      statusCode: 200
+      headers:
+        - cookie: ""
+      body: ""
+#      jsonMap:
+#        - key1: 1
+#        - key2: 2
+#    output:
+#      path: "."
+`)
+}
+
 func NewSignalContext() context.Context {
     ctx, cancel := context.WithCancel(context.Background())
     go func() {
@@ -22,8 +61,18 @@ func NewSignalContext() context.Context {
 }
 
 func main() {
+    version := flag.String("init", "", "初始化配置文件")
     cfgFilePath := flag.String("f", "", "配置文件")
     flag.Parse()
+    if version != "" {
+        switch version {
+        case "1":
+            CreateYaml()
+        default:
+            CreateYaml()
+        }
+        return
+    }
     if *cfgFilePath == "" {
         flag.PrintDefaults()
         log.Fatalf("未指定配置文件")
